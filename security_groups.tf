09aea449bd31a8d877cd0a426fcf66fdd44b9e7f
Tharun-Maheswararao <maheswararao.t@northeastern.edu>
2025-02-26 23:10:23 -0500

    create ec2

---
+++ security_groups.tf
@@ -0,0 +1,48 @@
+resource "aws_security_group" "application_sg" {
+  name        = "application-security-group"
+  description = "Allow web and SSH access"
+  vpc_id      = aws_vpc.main["vpc1"].id  # Choose your VPC (e.g., vpc1)
+
+  ingress {
+    description = "SSH Access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP Access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS Access"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Application Port"
+    from_port   = 8080  # Flask app default port (modify as required)
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "application-security-group"
+  }
+}
